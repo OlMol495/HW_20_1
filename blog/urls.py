f2613a2e599fe051b75c8ebba0bde99c7b701d5f
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2024-01-15 00:53:18 +0800

    Added post_form.html, corresponding path to blog.urls, fixed links to post_form in post_list.html, changed title in product_list.html, created PostCreateView in views.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import PostListView
+from blog.views import PostListView, PostCreateView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('list/', PostListView.as_view(), name='list'),
+    path('create/', PostCreateView.as_view(), name='create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from pytils.translit import slugify
 
 from blog.models import Post
 
 
 class PostListView(ListView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ('title', 'text', 'preview_image', 'is_published')
+    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+
+        return super().form_valid(form)
+class PostDetailView(DetailView):
+    model = Post
+
 
